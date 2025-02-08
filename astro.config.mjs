354b25e702efa58c23547dce8669c45521349ab7
ibsenseijas7 <ibsenseijas7@gmail.com>
2025-02-08 14:08:21 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://PortfolioIbsensDisena.github.io',
 	integrations: [tailwind()],
 });
